925b143322efa886e1e12d0f55abc7844c91ac4d
valeriabursuk <valeria.bursuk@gmail.com>
2022-11-03 17:08:28 -0400

    "setup"

---
+++ src/test/java/com/TryCloud/runner/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.TryCloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",  //normal html report
+                "rerun:target/rerun.txt",  //for rerun report
+                "me.jvt.cucumber.report.PrettyReports:/target/cucumber"},  //cucumber plugin for pretty reports
+
+        features = "src/test/resources/features",
+        glue = "com/TryCloud/step_definitions"
+        //dryRun = true
+      //  tags =
+
+)
+
+
+public class CukesRunner {
+}
