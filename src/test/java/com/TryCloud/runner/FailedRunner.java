2f89f2109f46f59f17607848d9d1e98a782d1757
valeriabursuk <valeria.bursuk@gmail.com>
2022-11-03 17:08:45 -0400

    setup

---
+++ src/test/java/com/TryCloud/runner/FailedRunner.java
@@ -0,0 +1,23 @@
+package com.TryCloud.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "@target/rerun.txt",
+        glue = "com/TryCloud/step_definitions"
+)
+
+
+
+
+
+
+public class FailedRunner {
+
+
+
+}
