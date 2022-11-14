4a7ea90c1a304ca2640758f4425a7b1a3448dcf7
FriyatMelaki <friyath@gmail.com>
2022-11-14 17:08:37 -0600

    US-04-Friyat

---
+++ src/test/java/com/TryCloud/pages/DashboardPage.java
@@ -0,0 +1,61 @@
+package com.TryCloud.pages;
+
+import com.TryCloud.utilities.BrowserUtils;
+import com.TryCloud.utilities.Driver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardPage {
+
+    public DashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li")
+    public List<WebElement> modules1;
+
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li[@id='more-apps']/preceding-sibling::li")
+    public List<WebElement> modules;
+
+
+    public List<String> getModulesText() {
+
+        List<String> modulesText = new ArrayList<>();
+        for (int i = 0; i < modules1.size() - 1; i++) {
+
+            BrowserUtils.hover(modules1.get(i));
+            BrowserUtils.sleep(1);
+            modulesText.add(modules1.get(i).getText());
+
+        }
+        return modulesText;
+
+    }
+
+    public void clickModulesWithText(String moduleName) {
+
+        for (int i = 0; i < modules1.size() - 1; i++) {
+
+            BrowserUtils.hover(modules1.get(i));
+            BrowserUtils.sleep(1);
+            if (modules1.get(i).getText().contains(moduleName)) {
+                BrowserUtils.hover(modules1.get(i));
+                modules1.get(i).click();
+
+                break;
+            }
+
+        }
+
+    }
+}
+
+
+
+
---
+++ src/test/resources/features/AccessFiles.feature
@@ -0,0 +1,17 @@
+Feature:
+@FM04
+  Scenario Outline: Test login with valid credentials
+    Given user on the login page
+    When user enters valid "<username>" and valid "<password>" and clicks login button
+    When the user clicks the "Files" module
+    Then verify the page title is "Files - Trycloud QA"
+    And user click the top-left checkbox of the table
+    Then verify all the files are selected
+
+    Examples:
+      | username | password    |
+      | User11   | Userpass123 |
+      | User41   | Userpass123 |
+      | User71   | Userpass123 |
+      | User101  | Userpass123 |
+
