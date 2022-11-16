53dad3ad97c65093e7204ef76ac6a2141e2c774e
Natalia <nata.vlasiuk17@gmail.com>
2022-11-16 12:35:41 -0600

    US 9 added

---
+++ src/test/java/com/TryCloud/pages/US9Page.java
@@ -0,0 +1,51 @@
+package com.TryCloud.pages;
+
+import com.TryCloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class US9Page {
+    public US9Page(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    //login:
+    @FindBy(xpath = "//input[@id='user']")
+    public WebElement userName;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
+    @FindBy(xpath = "//input[@id='submit-form']")
+    public WebElement loginBtn;
+
+
+    // My scenario:
+    @FindBy(xpath = "//ul[@id='appmenu']//a[@aria-label='Files']")
+    public WebElement filesModule;
+
+    @FindBy(xpath = "//a[@data-action='menu']//span[@class='icon icon-more']") //6 options
+    public List<WebElement> actionIconThreeDots;
+
+    @FindBy(xpath = "//a[@class='menuitem action action-details permanent']//span[.='Details']")
+    public  WebElement detailsOption;
+
+    @FindBy(xpath = "//a[@id='commentsTabView']")
+    public WebElement commentsBtn;
+
+    @FindBy(xpath = "//div[@data-placeholder='New comment …'] ")
+    public WebElement newCommentInputBox;
+
+    @FindBy(xpath = "//input[@class='submit icon-confirm has-tooltip']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//div[.='comment']")
+    public WebElement actualComment;
+
+    @FindBy(xpath = "(//div[@class='message'])[2]")
+    public WebElement commentToAppear;
+
+
+
+
+}
